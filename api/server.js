ee174df227ee8f3e1e09d21bba86b86adf3bda33
yujing <medischdossier03@gmail.com>
2024-03-17 15:26:27 +0100

    andere gegevens

--- api/server.js
+++ api/server.js
@@ -1,13 +1,6 @@
 const path = require('path')
 const express = require('express')
-// const fetchJson = require("../helpers/fetch-json.js");
 const app = express()
-
-
-// Haal data op uit de FDND API, ga pas verder als de data gedownload is
-const data = await fetchJson('https://fdnd.directus.app/items/person/9')// https://whois.fdnd.nl/admin/ mijn nummer is 9
-const datasquad = await fetchJson('https://fdnd.directus.app/items/squad/3')
-data.data.custom = JSON.parse(data.data.custom);/*parsen the string information naar data zoals de rest*/
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '../views'))
